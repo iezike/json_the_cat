01b236de26ee33fb16d37a1f540db47e344da86c
Ikenna John Ezike <ezike_iyke@yahoo.com>
2022-03-17 18:27:59 -0400

    created a new file to refactor breed fetcher

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const { fetchBreedDescription } = require('./breedFetcher');
+const breedName = process.argv[2];  // Catching command line input value.
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else if (desc) {
+    console.log(desc);
+  }
+});
